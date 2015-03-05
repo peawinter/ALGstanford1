324d6156e29b6f41f4b4a0523f1a37e6190cd308
Wenyu <peawinter@gmail.com>
2015-03-05 14:23:19 -0500

    Update prob6a
    -hastTable and search

--- prob6d.py
+++ prob6d.py
@@ -3,38 +3,42 @@
 import urllib2
 import sys
 
-def readData():
-    # return sorted data
-    url = 'https://d396qusza40orc.cloudfront.net/algo1%2Fprogramming_prob%2F2sum.txt'
-    file = urllib2.urlopen(url)
-    data = []
-    for line in file:
-        data.append(int(line))
-        # bisect.insort(bisect, int(line))
-    return data
-
-def TwoSum_HashTable(hashTable, lst, target):
-    for x in lst:
-        y = target-x
-        if y in hashTable and x != y:
-            return (x, y)
-        return None
-
-def main():
-    data = readData()
-    print data[:5]
+class Solution:
+    def readData(self):
+        # return sorted data
+        url = 'https://d396qusza40orc.cloudfront.net/algo1%2Fprogramming_prob%2F2sum.txt'
+        file = urllib2.urlopen(url)
+        data = []
+        for line in file:
+            data.append(int(line))
+            # bisect.insort(bisect, int(line))
+        return data
     
-    hashTable = dict()
-    for x in data:
-        hashTable[x] = True
-    print('size:' + str(len(hashTable)))
+    def hashData(self, data):
+        hashTable = dict()
+        for x in data:
+            hashTable[x] = True
+        print 'Size of hashtable', len(hashTable)
+        return hashTable
+    
+    def count2SumTarget(self, data, hashTable, target):
+        for x in data:
+            if target - x in hashTable:
+                return True
+        return False
+    
+    def count2SumAll(self):
+        data = self.readData()
+        print data[:10]
+        hashTable = self.hashData(data)
+        output = []
+        for target in range(-10000, 10001):
+            if self.count2SumTarget(data, hashTable, target):
+                output.append(target)
+        print output
+        return len(output)
         
-    count = 0
-    for t in range(-10000, 10000+1):
-        if(TwoSum_HashTable(hashTable, data, t)):
-            count += 1
-    print('Via hash table: ' + str(count))
-    
-
 if __name__ == '__main__':
-    main()
+    sol = Solution()
+    count = sol.count2SumAll()
+    print count
